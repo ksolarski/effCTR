d5b0b02fdf38c81bdbc741a6eeaffc6ed43b560b
kapa112 <pchla10@gmail.com>
2022-06-25 00:05:48 +0300

    update setup

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 
 
-setup(
-    name="effCTR",
-    version="0.0.1",
-    packages=find_packages(),
-)
+setup(install_requires=['numpy', 'scipy', 'sklearn'],
+      name="effCTR",
+      version="0.0.1",
+      packages=find_packages())

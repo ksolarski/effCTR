cf92835a32329292a5af77479c9579bb15cf6a90
kapa112 <pchla10@gmail.com>
2022-07-17 17:57:22 +0200

    sync setup with req from pipfile

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from setuptools import setup, find_packages
+import os.path
+_CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-setup(install_requires=['numpy', 'scipy', 'matplotlib'],
+def _parse_requirements(path):
+
+    with open(os.path.join(_CURRENT_DIR, path)) as f:
+        return [
+            line.rstrip()
+            for line in f
+            if not (line.isspace() or line.startswith(("#", "-i")))
+        ]
+
+
+_INSTALL_REQUIREMENTS = _parse_requirements(os.path.join(
+    _CURRENT_DIR, "requirements.txt"))
+
+
+setup(install_requires=_INSTALL_REQUIREMENTS,
       name="effCTR",
       version="0.0.1",
       packages=find_packages())
